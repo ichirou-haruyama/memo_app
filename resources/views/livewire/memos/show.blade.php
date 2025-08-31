213a5f34543c3474d3a8a4e0aa09e9580c34c4ad
ichirou-haruyama <ichirou.haruyama@gmail.com>
2025-08-31 14:31:56 +0900

    削除機能の追加

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -6,10 +6,16 @@ use App\Models\Memo;
 // ルートモデルバインディング
 state(['memo' =>  fn(Memo $memo) => $memo]);
 
-$edit = function(){
+$edit = function () {
     
     return redirect()->route('memos.edit', $this->memo);
-}
+};
+
+$destroy = function () {
+    $this->memo->delete();
+    return redirect()->route('memos.index');
+};
+
 ?>
 
 <div>
@@ -17,4 +23,5 @@ $edit = function(){
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(($memo->body)) !!}</p>
     <button wire:click="edit">編集する</button>
+    <button wire:click="destroy" wire:confirm="本当に削除しますか？">削除する</button>
 </div>
