08532c5d6e5a8e8d0287f6a4281e34a126ca1aa2
ichirou-haruyama <ichirou.haruyama@gmail.com>
2025-08-31 16:11:24 +0900

    エラーメッセージ色つけ

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -1,12 +1,18 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
 state(['title', 'body']);
 
+//バリテーションルールを定義
+    rules([
+        'title' => 'required|string|max:50',
+        'body' => 'required|string|max:2000',
+    ]);
 //メモを保存する関数
 $store = function() {
+    $this->validate(); //バリテーションチェック
     Memo::create([
         'title' => $this->title,
         'body' => $this->body,
@@ -21,11 +27,17 @@ $store = function() {
     <h1>新規登録</h1>
     <form wire:submit="store">
         <p>
-            <lavel for="title">タイトル</lavel><br>
+            <lavel for="title">タイトル</lavel>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror<br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror<br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
             <textarea wire:model="body" id="body"></textarea>
         </p>
